6125c686edc2756129bd717667c67d80481838f6
Elias Leite <elias.leite18@yahoo.com.br>
2020-05-06 00:33:20 -0300

    adicionado o nav_bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,30 @@
 <template>
   <div>
+
+<NavBar>
+  
+  
+   </NavBar>
+
     <nuxt />
   </div>
 </template>
+
+<script>
+
+import NavBar from "../components/NavBar"
+
+export default {
+
+components: {
+
+NavBar
+
+}
+  
+}
+</script>
+
 
 <style>
 html {
