cadea159db5c82984d2693ee153722e639b36e2c
Elias Leite <elias.leite18@yahoo.com.br>
2020-05-06 20:44:27 -0300

    modificado as cores e a fonte

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,27 +1,26 @@
 <template>
+  
   <el-menu
     :default-active="activeIndex"
     class="el-menu-demo"
     mode="horizontal"
     @select="handleSelect"
   >
-    <el-menu-item index="1">Processing Center</el-menu-item>
-    <el-submenu index="2">
-      <template slot="title">Workspace</template>
-      <el-menu-item index="2-1">item one</el-menu-item>
-      <el-menu-item index="2-2">item two</el-menu-item>
-      <el-menu-item index="2-3">item three</el-menu-item>
-      <el-submenu index="2-4">
-        <template slot="title">item four</template>
-        <el-menu-item index="2-4-1">item one</el-menu-item>
-        <el-menu-item index="2-4-2">item two</el-menu-item>
-        <el-menu-item index="2-4-3">item three</el-menu-item>
-      </el-submenu>
-    </el-submenu>
-    <el-menu-item index="3" disabled>Info</el-menu-item>
-    <el-menu-item index="4">
-      <a href="https://www.ele.me" target="_blank">Orders</a>
-    </el-menu-item>
+    <el-menu-item index ="0" > <img src= "ea.jpg" style="width:40px height:40px" > </el-menu-item>
+    
+    <el-menu-item index="1"
+    style="color:#FFF; font-family: 'Oswald', sans-serif; ">
+    O que fazemos </el-menu-item>
+
+      <el-menu-item index="2"
+        style="color:#FFF; font-family: 'Oswald', sans-serif; ">
+        Nosso Produto</el-menu-item>
+        
+        <el-menu-item index="3"
+          style="color:#FFF; font-family: 'Oswald', sans-serif;">
+            <a style=" text-decoration: none;" href="https://www.ele.me" target="_blank">Orders</a>
+              </el-menu-item>
+   
   </el-menu>
 </template>
 
@@ -30,8 +29,16 @@ export default {
      name: "NavBar_P"
 
 }
+
 </script>
 
 <style>
+
+@import url('https://fonts.googleapis.com/css2?family=Oswald&display=swap');
+
+.el-menu-demo{
+background:#2A7AE4;
+}
+
 
 </style>
