b4a10e0d47616d3c8496381dbf62464f0a63c64e
Freitas29 <arao.freitas@hotmail.com>
2020-05-08 00:23:00 -0300

    Chart loading

--- pages/Chart.vue
+++ pages/Chart.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>    <Brazil />
+  <div>
+    <Brazil v-if="!loading" />
+
+    <transition name="fade">
+      <div class="animation-loading" v-if="loading">
+        <lottie-player
+          src="https://assets1.lottiefiles.com/packages/lf20_x62chJ.json"
+          background="transparent"
+          speed="1"
+          style="width: 500px; height: 500px;"
+          loop
+          autoplay
+        />
+      </div>
+    </transition>
     <el-dialog title="Filtro" :visible.sync="dialogVisible" width="30%">
       <span
         >Deseja pesquisar pelo estado de <b>{{ stateName }}</b
@@ -16,6 +30,7 @@
 
 <script>
 import Brazil from '~/components/Brazil'
+import '@lottiefiles/lottie-player'
 
 export default {
   name: 'Chart',
@@ -23,15 +38,17 @@ export default {
     return {
       dialogVisible: false,
       stateName: '',
+      loading: false
     }
   },
   mounted() {
     this.$root.$on('stateClicked', this.handleStateClicked)
-    
+
     gapi.load('client:auth2', function() {
       gapi.auth2.init({
-        client_id: '963954958594-6pageg21t18elik8up5nr2k2f0h2a4u8.apps.googleusercontent.com',
-        scope: "https://www.googleapis.com/auth/bigquery.readonly"
+        client_id:
+          '963954958594-6pageg21t18elik8up5nr2k2f0h2a4u8.apps.googleusercontent.com',
+        scope: 'https://www.googleapis.com/auth/bigquery.readonly'
       })
     })
   },
@@ -39,6 +56,7 @@ export default {
     authenticate() {
       const vmo = this
 
+      this.loading = true
       return gapi.auth2
         .getAuthInstance()
         .signIn({
@@ -46,7 +64,6 @@ export default {
         })
         .then(
           async function(resp) {
-            
             const { access_token } = resp.getAuthResponse()
 
             const response = await vmo.$axios.get(
@@ -59,8 +76,11 @@ export default {
               }
             )
 
-            const { data: { rows } } = response
+            const {
+              data: { rows }
+            } = response
 
+            vmo.loading = false
           },
           function(err) {
             console.error('Error signing in', err)
@@ -76,9 +96,11 @@ export default {
       this.stateName = name
       this.dialogVisible = true
     },
-    clearString(estado){
-      const normalize  = estado.normalize('NFD').replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, '');
-      return normalize.replace(/[^\w\s]/gi, "")
+    clearString(estado) {
+      const normalize = estado
+        .normalize('NFD')
+        .replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, '')
+      return normalize.replace(/[^\w\s]/gi, '')
     }
   },
   components: {
@@ -91,5 +113,21 @@ export default {
 .chart {
   width: 100%;
   height: 500px;
+}
+
+.animation-loading {
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(29, 53, 87, 0.363);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
