8e3099198b5037edf7140a9c3a13079b40d60c25
Freitas29 <arao.freitas@hotmail.com>
2020-10-17 16:13:14 -0300

    Atualiza meta tag e logo

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,10 +9,11 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { hid: 'description', name: 'description', content: 'Buscamos os melhores planos de saúde por cada região do Brasil' },
+      { property: 'og:image', content: "./assets/pingo.png" || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/pingo.ico' }
     ]
   },
   env: {
